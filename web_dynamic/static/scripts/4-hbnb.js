63edeb593f2f3ce7e76de94f17d8df7121fc6256
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 <l.leroty@gmail.com>
2022-12-21 17:12:56 +0100

    update the filters!!!!!

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -46,13 +46,37 @@ $(document).ready(function () {
             <div class="number_bathrooms">${data[i].number_bathrooms}</div>
 	  </div>
 	  <div class="user">
-            <b>Owner:</b>${data[i].user.first_name} ${data[i].user.last_name}
-          </div>
-          <div class="description">
-	    ${data[i].description}
-          </div>
+
 	</article>
         `)};
       }
       });
+
+      $('button').click(function () {
+        $('section.places').empty();
+        $.ajax({
+          type: 'POST',
+            url: 'http://0.0.0.0:5001/api/v1/places_search/',
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            data: JSON.stringify({amenities: Object.keys(amenities)}),
+            success: function(data) {
+              console.log(data);
+              for (let i = 0; i < data.length; i++) {
+                $('section.places').append(`
+                <article>
+          <div class="title_box">
+            <h2>${data[i].name}</h2>
+            <div class="price_by_night">$${data[i].price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${data[i].max_guest}</div>
+                  <div class="number_rooms">${data[i].number_rooms}</div>
+                  <div class="number_bathrooms">${data[i].number_bathrooms}</div>
+          </div>
+          <div class="user">
+        </article>
+              `)};
+      }});
+      })
 });
